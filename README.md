b0f9fc6daeeadbf54fde562d4e71936fb2414355
Austin Workman <austin@hashicorp.com>
2022-06-30 23:14:35 -0500

    Adding prefix, contains options, and increasing output clarity

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Dero Wallet Generator
 
-This application will allow you to supply a suffix and generate wallet keys until a
-matching suffix is located.  Once you've located the keys a seed output will be presented
+This application will allow you to supply a suffix/prefix and generate wallet keys until a
+matching suffix/prefix is located.  Once you've located the keys a seed output will be presented
 which can be used to restore your wallet in the official wallet client.  
 
 The process of searching for a wallet suffix is similar to mining where more complex 
@@ -19,14 +19,18 @@ dero1qyxg6dmw22xh9v0hkp3xm6cns6qn9cwl9zp25mxxwmy7mvdea7akyqgetluck
 Generate Dero Wallet with matching suffix
 
 Usage:
-dero-wallet-gen --suffix=<suffix>
-dero-wallet-gen -h | --help
+  dero-wallet-gen --suffix=<string>
+  dero-wallet-gen --prefix=<string>
+  dero-wallet-gen --contains=<string>
+  dero-wallet-gen -h | --help
 
 Options:
--h --help           Show this screen.
---suffix=<suffix>   Search for wallet with this string suffix
+  -h --help           Show this screen.
+  --suffix=<string>   Search for wallet with this string suffix
+  --prefix=<string>   Search for wallet with this string prefix
+  --contains=<string> Search for wallet with this string anywhere
 
-Example: ./dero-wallet-gen --suffix getluck
+Example: ./dero-wallet-gen --suffix dead
 ```
 
 # Building
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"github.com/deroproject/derohe/walletapi"
 	"github.com/docopt/docopt-go"
@@ -8,9 +9,12 @@ import (
 	"os/signal"
 	"runtime"
 	"strings"
+	"sync"
 )
 
-var suffix string
+var suffix = ""
+var prefix = ""
+var contains = ""
 var arguments = map[string]interface{}{}
 var exit = make(chan bool)
 
@@ -18,45 +22,65 @@ var command_line string = `dero-wallet-gen
 Generate Dero Wallet with matching suffix
 
 Usage:
-  dero-wallet-gen --suffix=<suffix>
+  dero-wallet-gen --suffix=<string>
+  dero-wallet-gen --prefix=<string>
+  dero-wallet-gen --contains=<string>
   dero-wallet-gen -h | --help
 
 Options:
   -h --help           Show this screen.
-  --suffix=<suffix>   Search for wallet with this string suffix
+  --suffix=<string>   Search for wallet with this string suffix
+  --prefix=<string>   Search for wallet with this string prefix
+  --contains=<string> Search for wallet with this string anywhere
 
 Example: ./dero-wallet-gen --suffix dead
+
 `
+var threadwrite = sync.Mutex{}
 
 func main() {
 	arguments, _ = docopt.Parse(command_line, nil, true, "v0.0.1", false)
 	if arguments["--suffix"] != nil {
 		suffix = arguments["--suffix"].(string)
+	} else if arguments["--prefix"] != nil {
+		prefix = arguments["--prefix"].(string)
+	} else if arguments["--contains"] != nil {
+		contains = arguments["--contains"].(string)
 	} else {
 		return
 	}
+
 	threads := runtime.GOMAXPROCS(0)
 	go func() {
 		var gracefulStop = make(chan os.Signal, 1)
 		signal.Notify(gracefulStop, os.Interrupt)
 		for {
 			sig := <-gracefulStop
-			fmt.Printf("\nReceived signal %s\n", sig)
+			fmt.Fprintf(os.Stderr, "\nReceived signal %s\n", sig)
 
 			if sig.String() == "interrupt" {
 				close(exit)
 			}
 		}
 	}()
+	fmt.Fprintf(os.Stderr, "Generating wallets....this could take some time.\n")
 
 	for i := 0; i < threads; i++ {
-		go findWallet(suffix)
+		if suffix != "" {
+			go findWalletSuffix(suffix)
+		} else if prefix != "" {
+			go findWalletPrefix(prefix)
+		} else if contains != "" {
+			go findWalletContains(contains)
+		} else {
+			return
+		}
 	}
 	<-exit
 	return
 }
 
-func findWallet(suffix string) {
+func findWalletSuffix(suffix string) {
 	for {
 		account, _ := walletapi.Generate_Keys_From_Random()
 		account.SeedLanguage = "English"
@@ -69,12 +93,44 @@ func findWallet(suffix string) {
 	}
 }
 
+func findWalletPrefix(prefix string) {
+	for {
+		account, _ := walletapi.Generate_Keys_From_Random()
+		account.SeedLanguage = "English"
+		address := account.GetAddress()
+		address.Mainnet = true
+		mainaddr := address.String()
+		if strings.HasPrefix(mainaddr, ("dero1qy" + prefix)) {
+			getSeeds(account)
+		}
+	}
+}
+
+func findWalletContains(contains string) {
+	for {
+		account, _ := walletapi.Generate_Keys_From_Random()
+		account.SeedLanguage = "English"
+		address := account.GetAddress()
+		address.Mainnet = true
+		mainaddr := address.String()
+		if strings.Contains(mainaddr, contains) {
+			getSeeds(account)
+		}
+	}
+}
+
 func getSeeds(account *walletapi.Account) {
+	bytes := account.Keys.Public.EncodeCompressed()
+	private := account.Keys.Secret.String()
 	w, err := walletapi.Create_Encrypted_Wallet_Memory("yes", account.Keys.Secret)
 	if err == nil {
 		address := w.GetAddress()
 		address.Mainnet = true
-		fmt.Println(address)
+		threadwrite.Lock()
+		defer threadwrite.Unlock()
+		fmt.Println(fmt.Sprintf("%-10s %s", "Public: ", hex.EncodeToString(bytes[:])))
+		fmt.Println(fmt.Sprintf("%-10s %s", "Private: ", private))
+		fmt.Println(fmt.Sprintf("%-10s %s", "Wallet: ", address))
 		seed := w.GetSeed()
 		fmt.Println(seed, "\n")
 	}
